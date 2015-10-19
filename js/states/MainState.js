195011f78b8f0aa8a3c599539ca58c2da36904c0
Jordy Moos <jordymoos@gmail.com>
2015-10-19 21:49:41 +0200

    Gems running to their spot

--- js/states/MainState.js
+++ js/states/MainState.js
@@ -32,6 +32,51 @@ var MainState = {
 
         var seed = Date.now();
         this.random = new Phaser.RandomDataGenerator([seed]);
+
+        this.initTiles();
+    },
+
+    initTiles: function ()
+    {
+        for (var y = 0; y < this.tileGrid.length; ++y)
+        {
+            for (var x = 0; x < this.tileGrid.length; ++x)
+            {
+                this.tileGrid[y][x] = this.addTile(x, y);
+            }
+        }
+
+        this.game.time.events.add(600, function ()
+            {
+                this.checkMatch();
+            }, this);
+    },
+
+    addTile: function (x, y)
+    {
+        var tileToAdd = this.tileTypes[this.random.integerInRange(0, this.tileTypes.length -1)];
+
+        var tile = this.tiles.create((x * this.tileWidth) + (this.tileWidth / 2) + ((x + 1) * 10), 0, tileToAdd);
+        tile.anchor.setTo(0.5, 0.5);
+        tile.inputEnabled = true;
+        tile.tileType = tileToAdd;
+        tile.events.onInputDown.add(this.tileDown, this);
+
+        this.game.add.tween(tile).to({
+                y: (y * this.tileHeight) + (this.tileHeight / 2) + ((y + 1) * 10)
+            }, 500, Phaser.Easing.Linear.In, true);
+
+        return tile;
+    },
+
+    tileDown: function (tile, poointer)
+    {
+
+    },
+
+    checkMatch: function ()
+    {
+        console.log('Check match');
     },
 
     update: function ()
